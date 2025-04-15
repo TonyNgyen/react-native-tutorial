7255515035195cfeea78e6ddb0e8b44b312dd465
Tony Nguyen <tonynguyeenn@gmail.com>
2025-04-14 22:27:05 -0700

    Adding list of events

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,17 @@
 import { Stack } from 'expo-router';
-import { View, Text } from 'react-native';
+import EventListItem from '~/components/EventListItem';
+import events from '~/assets/events.json';
+import { FlatList } from 'react-native';
 
 export default function Events() {
   return (
     <>
       <Stack.Screen options={{ title: 'Events' }} />
+      <FlatList
+        className="bg-white"
+        data={events}
+        renderItem={({ item }) => <EventListItem event={item} />}
+      />
     </>
   );
 }
