ce03a5aa05a49f73e4650b55c4eb7f033ba63b01
Tony Nguyen <tonynguyeenn@gmail.com>
2025-04-16 18:42:27 -0700

    Added auth and profile data

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,7 +1,7 @@
 import dayjs from 'dayjs';
 import { useLocalSearchParams, Stack } from 'expo-router';
 
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Pressable } from 'react-native';
 import events from '~/assets/events.json';
 
 export default function EventPage() {
@@ -15,9 +15,8 @@ export default function EventPage() {
       <Stack.Screen
         options={{
           title: 'Event',
-          headerBackTitleStyle: {
-            fontSize: 1,
-          },
+          headerBackButtonDisplayMode: 'minimal',
+          headerTintColor: 'black',
         }}
       />
       <Image source={{ uri: event.image }} className="aspect-video w-full rounded-xl" />
@@ -30,6 +29,13 @@ export default function EventPage() {
       <Text className="text-lg" numberOfLines={2}>
         {event.description}
       </Text>
+      {/* Footer */}
+      <View className="absolute bottom-0 left-0 right-0 flex-row items-center justify-between border-t-2 border-gray-300 p-5 pb-10">
+        <Text className="text-xl font-semibold">Free</Text>
+        <Pressable className="rounded-md bg-red-500 p-5 px-8">
+          <Text className="text-lg font-bold text-white">Join and RSVP</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
